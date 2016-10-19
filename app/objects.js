ea7a0f48ff0a72c556f0d7329aed9c2edeb64175
Niles McGiver <niles@finedesigngroup.com>
2016-10-18 19:07:43 -0700

    got the specs for objects to pass

--- app/objects.js
+++ app/objects.js
@@ -1,15 +1,22 @@
 exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
-  alterContext: function(fn, obj) {
 
+  alterContext: function(fn, obj) {
+    return fn.call(obj);
   },
 
   alterObjects: function(constructor, greeting) {
-
+    return constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
-
+    let owned = [];
+    for (key in obj) {
+      if ( obj.hasOwnProperty(key) ) {
+        owned.push(key + ': ' + obj[key]);
+      }
+    }
+    return owned;
   }
 };
